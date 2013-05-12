40e88f82239632da0ac969ad8f236805c147c239
Harun Yardimci <hyardimci@ebay.com>
2013-05-12 14:08:35 +0300

    AOP

---
+++ src/main/java/com/gg/petclinic/aop/PetClinicTrackerAspect.java
@@ -0,0 +1,42 @@
+package com.gg.petclinic.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+/**
+ * User: { "id": "hyardimci", "email":nosqlsolutions@gmail.com"}
+ * Date: 5/12/13
+ * Time: 1:46 PM
+ */
+@Aspect
+public class PetClinicTrackerAspect {
+
+    @Around("execution (* com.gg..*.*(..))")
+    public Object trace(ProceedingJoinPoint pjp) throws Throwable  {
+
+        try {
+            System.out.println("Before AOP :" + pjp.getSignature().getDeclaringTypeName());
+
+            return pjp.proceed();
+        }
+        finally {
+            System.out.println("After AOP:" + pjp.getSignature());
+        }
+    }
+
+
+    @Around("bean(*Service)")
+    public Object trace2(ProceedingJoinPoint pjp) throws Throwable  {
+
+        try {
+            System.out.println("Before AOP with XML :" + pjp.getSignature().getDeclaringTypeName());
+
+            return pjp.proceed();
+        }
+        finally {
+            System.out.println("After AOP with XML :" + pjp.getSignature());
+        }
+    }
+
+}
